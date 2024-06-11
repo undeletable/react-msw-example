52cb4f73a3ea17aa18afb2d316517aed09e90f5c
Max Shepel <max@undeletable.name>
2024-06-11 14:53:48 +0300

    Add handlers and data display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import React, { useState } from 'react';
 import './App.css';
+import { getAllProducts, getMultipleProducts, getSingleProduct } from './requests';
 
 function App() {
+  const [output, setOutput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  
+  const requestAndSetData = (requestFunction) => {
+    setIsError(false);
+    setIsLoading(true);
+    requestFunction()
+      .then(response => setOutput(response))
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }
+
   return (
     <div className="App">
+      <div className="App__buttons">
+        <button onClick={() => requestAndSetData(getAllProducts)}>
+          Fetch all products
+        </button>
+        <button onClick={() => requestAndSetData(getSingleProduct(1))}>
+          Fetch product #1
+        </button>
+        <button onClick={() => requestAndSetData(getMultipleProducts(1, 2, 3))}>
+          Fetch products #1, #2, #3
+        </button>
+      </div>
+      <div className="App__result">
+        {isLoading ? (
+          'Loading...'
+        ) : (
+          <code>
+            {isError ? 'Error' : JSON.stringify(output, null, 2)}
+          </code>
+        )}
+      </div>
     </div>
   );
 }
--- src/mock-api/handlers.js
+++ src/mock-api/handlers.js
@@ -1,1 +1,23 @@
-export const handlers = [];
+import { http, HttpResponse } from 'msw';
+import products from './data/products.json';
+
+const getJSONResponse = data =>
+    HttpResponse.json({
+      data,
+    });
+
+export const handlers = [
+  http.get(
+    '/api/products',
+    () => getJSONResponse(products)
+  ),
+  http.get(
+    '/api/products/:ids',
+    ({ params }) => {
+        const { ids } = params;
+        const idsArray = ids.split(',');
+        const filteredProducts = products.filter(({ id }) => idsArray.includes(id.toString()));
+        return getJSONResponse(filteredProducts);
+    }
+  )
+];
